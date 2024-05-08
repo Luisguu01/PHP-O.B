9ced989805ea323777be6f5d382f8fec4df41d90
Lgxx2 <143849971+Luisguu01@users.noreply.github.com>
2024-05-07 21:39:37 -0300

    Create Produto.php

---
+++ Produto.php
@@ -0,0 +1,24 @@
+<?php
+class Produto{
+    //atributos classe
+    public $descricao;
+    public $estoque;
+    public $preco;
+
+public function aumentarEstoque($unidades){
+    if(is_numeric($unidades) AND $unidades > 0){
+        $this->estoque += $unidades;
+    }
+}
+public function diminuirEstoque($unidades){
+    if(is_numeric($unidades) AND $unidades > 0){
+        $this->estoque -= $unidades;
+    }
+}
+public function reajustePreco($percentual){
+    if(is_numeric($percentual) AND $percentual != 0){
+        $this-> preco *= (1+($percentual/100));
+    }
+}
+}
+?>
