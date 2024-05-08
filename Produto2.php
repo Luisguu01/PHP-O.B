dfeef7b94e8cafc79ba61bdbe73650742ef9f728
Lgxx2 <143849971+Luisguu01@users.noreply.github.com>
2024-05-07 21:40:12 -0300

    Create Produto2.php

---
+++ Produto2.php
@@ -0,0 +1,57 @@
+<?php
+class Produto2{
+ 
+    private $descricao;
+    private $estoque;
+    private $preco;
+
+    public function __construct($descricao, $estoque, $preco){
+        $this->descricao = $descricao;
+        $this->estoque = $estoque;
+        $this->preco = $preco;
+    }
+
+    public function __destruct(){
+      //  print "<p>O produto {$this->descricao} foi destruido! </p>";
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+    public function getEstoque(){
+        return $this -> estoque;
+    }
+
+    public function setEstoque($estoque){
+        $this->estoque = $estoque;
+    }
+
+    public function getPreco(){
+        return $this -> preco;
+    }
+
+    public function setPreco($preco){
+        $this->preco = $preco;
+    }
+
+public function aumentarEstoque($unidades){
+    if(is_numeric($unidades) AND $unidades > 0){
+        $this->estoque += $unidades;
+    }
+}
+public function diminuirEstoque($unidades){
+    if(is_numeric($unidades) AND $unidades > 0){
+        $this->estoque -= $unidades;
+    }
+}
+public function reajustePreco($percentual){
+    if(is_numeric($percentual) AND $percentual != 0){
+        $this-> preco *= (1+($percentual/100));
+    }
+}
+}
+?>
